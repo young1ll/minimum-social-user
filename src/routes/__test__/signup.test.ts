33458379f8542c6a01271f658e11c44c5dc55b53
young1ll <122770896+young1ll@users.noreply.github.com>
2024-01-09 21:01:21 +0900

    feat password validation #6

--- src/routes/__test__/signup.test.ts
+++ src/routes/__test__/signup.test.ts
@@ -1,24 +1,90 @@
 import request from 'supertest';
 import app from '@/app';
 
-// it('Return 405 for non-post request to signup', async () => {
-//   await request(app).get('/').expect(200);
-// });
+/**
+ * Valid Email conditions:
+ *
+ * - Standard Email format from 'express-validator' pkg
+ * 422: Unprocessable Entity
+ */
+describe('Test Validity of Email', () => {
+  let password: string;
+  beforeAll(async () => {
+    password = 'validPassw0rd';
+  });
 
-// 422: Unprocessable Entity
-it('Return 422 if the email is not valid', async () => {
-  await request(app)
-    .post('/api/auth/signup') // prettier-ignore
-    .send({})
-    .expect(422);
+  it('Return 422 if the email is not provided', async () => {
+    await request(app).post('/api/auth/signup').send({ password }).expect(422);
+  });
 
-  await request(app)
-    .post('/api/auth/signup')
-    .send({ email: 'invalid email' })
-    .expect(422);
+  it('Return 422 if the email is not valid', async () => {
+    await request(app)
+      .post('/api/auth/signup')
+      .send({ email: 'Invalid email', password })
+      .expect(422);
+  });
 
-  await request(app)
-    .post('/api/auth/signup')
-    .send({ password: 'invalid email' })
-    .expect(422);
+  it('Return 200 if the email is valid', async () => {
+    await request(app)
+      .post('/api/auth/signup')
+      .send({ email: 'valid@email.com', password })
+      .expect(200);
+  });
+});
+
+/**
+ * Valid Password conditions:
+ *
+ * - At least 6 characters
+ * - At most 32 characters
+ * - Must contain at least one uppercase letter
+ * - Must contain at least one lowercase letter
+ * - Must contain at least one number
+ */
+describe('Test Validity of Password', () => {
+  let email: string;
+  beforeAll(async () => {
+    email = 'valid@email.com';
+  });
+
+  it('Return 422 if the password is not provided', async () => {
+    await request(app).post('/api/auth/signup').send({ email }).expect(422);
+  });
+  it('Return 422 if the password contains less than 6 characters', async () => {
+    await request(app)
+      .post('/api/auth/signup')
+      .send({ email, password: 'vaLid' })
+      .expect(422);
+  });
+  it('Return 422 if the password contains more than 32 characters', async () => {
+    await request(app)
+      .post('/api/auth/signup')
+      .send({ email, password: 'vaLi5_Pas10_0rd15_ida20_dfi25_dfa30_asd35' })
+      .expect(422);
+  });
+  it('Return 422 if the password does not contains any uppercase', async () => {
+    await request(app)
+      .post('/api/auth/signup')
+      .send({ email, password: 'valid2' })
+      .expect(422);
+  });
+  it('Return 422 if the password does not contains any lowercase', async () => {
+    await request(app)
+      .post('/api/auth/signup')
+      .send({ email, password: 'VALID2' })
+      .expect(422);
+  });
+  it('Return 422 if the password does not contains any number', async () => {
+    await request(app)
+      .post('/api/auth/signup')
+      .send({ email, password: 'vaLiiD' })
+      .expect(422);
+  });
+
+  it('Return 200 if the password is valid', async () => {
+    await request(app)
+      .post('/api/auth/signup')
+      .send({ email, password: 'vaLid2' })
+      .expect(200);
+  });
 });
--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -5,7 +5,22 @@ const signUpRouter = express.Router();
 
 signUpRouter.post(
   '/api/auth/signup',
-  [body('email').isEmail().withMessage('Email must be valid')],
+  [
+    body('email').trim().isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .isLength({ min: 6, max: 32 })
+      .withMessage('Password must be between 6 and 32 characters'),
+    body('password')
+      .matches(/^(.*[a-z].*)$/)
+      .withMessage('Password must contain at least one lowercase character'),
+    body('password')
+      .matches(/^(.*[A-Z].*)$/)
+      .withMessage('Password must contain at least one uppercase character'),
+    body('password')
+      .matches(/^(.*\d.*)$/)
+      .withMessage('Password must contain at least one number'),
+  ],
   (req: Request, res: Response) => {
     const errors = validationResult(req);
 
