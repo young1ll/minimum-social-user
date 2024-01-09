acd4d8120e5200688c49f5a03ba61ef50b8e8f30
young1ll <122770896+young1ll@users.noreply.github.com>
2024-01-09 14:36:21 +0900

    test jest and husky success

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,22 @@ module.exports = {
     es6: true,
     browser: true,
   },
+  env: {
+    jest: true,
+  },
+  ignorePatterns: ['node_modules/'],
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
   rules: {
+    'no-console': 'off',
     'prettier/prettier': ['error', { endOfLine: 'auto' }],
     'brace-style': ['error', 'stroustrup'],
     'comma-dangle': ['error', 'always-multiline'],
     'no-unused-vars': ['warn'],
+    'import/extensions': ['error', { js: 'never' }],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
   },
 };
