3a63970cd636a585ac6c352b86ba3f5e6897747c
young1ll <122770896+young1ll@users.noreply.github.com>
2024-01-11 18:18:08 +0900

    rename

--- src/controllers/signup-controller.ts
+++ src/controllers/signup-controller.ts
@@ -1,14 +1,18 @@
+import config from '@/config';
 import InvalidInputError from '@/errors/invalid-error';
 import { UserSignedUpEv } from '@/event';
 import { UserService } from '@/services';
+import MailgunService from '@/services/mailgun-service';
 import { ISignupDTO } from '@/services/user-service';
 import { Request, Response } from 'express';
 import { validationResult } from 'express-validator';
 
+// NOTE: trycatch 안써도 괜찮은가?
 export const SignUpController = async (req: Request, res: Response) => {
   const errors = validationResult(req).array();
 
   const userService = new UserService();
+  const mailgunService = new MailgunService(config.mailgun.apiKey!);
 
   if (/.*@[A-Z]/g.test(req.body.email)) {
     errors.push({
@@ -34,13 +38,22 @@ export const SignUpController = async (req: Request, res: Response) => {
     throw new InvalidInputError(errors);
   }
 
-  const { email, password }: ISignupDTO = req.body;
+  const { username, email, password, otp }: ISignupDTO = req.body;
 
-  const userSignUp = await userService.signUp(
-    req.body.username,
+  const userSignUp = await userService.signUp({
+    username,
     email,
     password,
-  );
+    otp,
+  });
+
+  const response = await mailgunService.sendMailFromAdmin({
+    domain: config.mailgun.domain!,
+    toEmail: email,
+    subject: 'Welcome to Minimum Socials',
+    message: `Welcome ${username}!`,
+  });
+
   const signupEvent = new UserSignedUpEv(userSignUp);
 
   return res
