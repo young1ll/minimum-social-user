216b37a585e7edf4a2a6cb02562e8aee79a27e0e
young1ll <122770896+young1ll@users.noreply.github.com>
2024-01-31 23:22:38 +0900

    chore docker commands

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -4,7 +4,12 @@ echo "> USER-SERVER REPOSITORY: $REPOSITORY"
 
 cd $REPOSITORY
 
+# Docker Stop & Remove
 docker stop user-server
 docker rm user-server
+
+# Docker Build
 docker build -t user-server .
+
+# Docker Run
 docker run -d -p 8000:8000 user-server
