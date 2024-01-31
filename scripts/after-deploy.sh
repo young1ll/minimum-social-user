cbc33f423ba28f660e5f23e726c02be660e2c614
young1ll <122770896+young1ll@users.noreply.github.com>
2024-02-01 02:03:29 +0900

    please...please...please...please...please

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -5,11 +5,11 @@ echo "> USER-SERVER REPOSITORY: $REPOSITORY"
 cd $REPOSITORY
 
 # 기존 컨테이너 중지 및 삭제
-sudo docker stop user-server
-sudo docker rm user-server
+docker stop user-server
+docker rm user-server
 
 # Docker Build
-sudo docker build -t user-server:latest .
+docker build -t user-server:latest .
 
 # Docker Run
-sudo docker run -d -p 8000:8000 --name user-server user-server:latest
+docker run -d -p 8000:8000 --name user-server user-server:latest
