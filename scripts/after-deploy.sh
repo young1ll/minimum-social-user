b2e72615c8af93312d8598cdd4acb6786a33519d
young1ll <122770896+young1ll@users.noreply.github.com>
2024-02-01 00:18:36 +0900

    fix file_exists_behavior conflict + sh permission

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -4,12 +4,12 @@ echo "> USER-SERVER REPOSITORY: $REPOSITORY"
 
 cd $REPOSITORY
 
-# Docker Stop & Remove
+# 기존 컨테이너 중지 및 삭제
 sudo docker stop user-server
 sudo docker rm user-server
 
 # Docker Build
-sudo docker build -t user-server .
+sudo docker build -t user-server:latest .
 
 # Docker Run
-sudo docker run -d -p 8000:8000 user-server
+sudo docker run -d -p 8000:8000 --name user-server user-server:latest
